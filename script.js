539d8b13efa0232973854b71f628bca03255cd4c
akshaygurme <42416798+akshaygurme@users.noreply.github.com>
2022-05-04 01:59:33 +0530

    Leaflet library implemented

--- script.js
+++ script.js
@@ -10,3 +10,29 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { longitude } = position.coords;
+      const { latitude } = position.coords;
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+      const coords = [latitude, longitude];
+
+      let map = L.map('map').setView(coords, 17);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+    },
+    function () {
+      alert('could not get your location');
+    }
+  );
